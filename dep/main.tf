2e3f6158100820076dbebe6ff7efc036f47aa02f
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-09-30 17:14:45 -0300

    Advanced feature - Data Source - part2

--- dep/main.tf
+++ dep/main.tf
@@ -6,7 +6,12 @@ provider "aws" {
 resource "aws_instance" "db" {
   ami           = "ami-03a6eaae9938c858c"
   instance_type = "t2.micro"
+  tags = {
+    Name = "terraform-db"
+  }
 }
+
+
 
 
 resource "aws_instance" "web" {
@@ -14,4 +19,15 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   depends_on = [aws_instance.db]
+}
+
+data "aws_instance" "dbsearch" {
+  filter {
+    name   = "tag:Name"
+    values = ["terraform-db"]
+  }
+}
+
+output "dbservers" {
+  value = data.aws_instance.dbsearch.availability_zone
 }
